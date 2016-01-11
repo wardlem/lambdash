26aa29872d313d4d0c3639c9e86fd1439a6a6ad1
mwardle <mwardle@robinsage.com>
2016-01-10 21:27:37 -0700

    Added functionality and tests to DT

--- src/DT.js
+++ src/DT.js
@@ -1,3 +1,27 @@
+var _curry = require('./internal/_curry');
+
+var Ordering = require('./Ordering');
+
 var DT = require('./internal/_primitives').DT;
 
 module.exports = DT;
+
+DT.eq = _curry(function(left, right) {
+    return +left === +right;
+});
+
+DT.compare = _curry(function(left, right){
+    return Ordering.fromNum(left - right);
+});
+
+DT.toNum = _curry(function(dt){
+    return +dt;
+});
+
+DT.fromNum = _curry(function(num){
+    return new Date(num);
+});
+
+DT.show = _curry(function(dt){
+    return "Date('" + dt.toUTCString() + "')";
+});
