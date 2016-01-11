b62afc42f5771f25ebee0d46517be3ec8a96109c
mwardle <mwardle@robinsage.com>
2016-01-10 21:58:23 -0700

    Minor updates to enumeratedType unit test

--- test/enumeratedType.mocha.js
+++ test/enumeratedType.mocha.js
@@ -22,17 +22,41 @@ var DAYS = enumeratedType("DAYS", [
 
 describe('enumeratedType', function(){
 
+    it('should accept a function as the first parameter', function(){
+        var Constr = function Constr(int) {
+            return Constr.fromInt(int);
+        };
+
+        var Constr2 = enumeratedType(Constr, ["A", "B", "C"]);
+
+        assert(Constr === Constr2);
+        assert(Constr.A);
+        assert(Constr.B);
+        assert(Constr.C);
+    });
+
+    it('should throw an exception if called without an array as the second parameter', function(){
+        try {
+            var ENUM = enumeratedType("ENUM", {});
+            assert(false);
+        } catch (e) {
+            assert(e instanceof TypeError);
+        }
+    });
+
     it('should create a sum type that is convertible to and from an integer value', function(){
         assert.equal(DAYS.toInt(DAYS.TUESDAY), 2);
         assert.equal(DAYS.fromInt(5), DAYS.FRIDAY);
     });
 
     it('should create a sum type that implements Eq', function() {
+        assert(Eq.member(DAYS.SUNDAY));
         assert.equal(Eq.eq(DAYS.SUNDAY, DAYS.SUNDAY), true);
         assert.equal(Eq.eq(DAYS.SUNDAY, DAYS.MONDAY), false);
     });
 
     it('should create a sum type that implements Bounded', function() {
+        assert(Bounded.member(DAYS.SUNDAY));
         assert.equal(Bounded.isMin(DAYS.SUNDAY), true);
         assert.equal(Bounded.isMax(DAYS.SUNDAY), false);
         assert.equal(Bounded.isMin(DAYS.WEDNESDAY), false);
@@ -42,12 +66,16 @@ describe('enumeratedType', function(){
     });
 
     it('should create a sum type that implements Ord', function() {
+        assert(Ord.member(DAYS.SUNDAY));
+
         assert(Ordering.isEQ(Ord.compare(DAYS.SUNDAY, DAYS.SUNDAY)));
         assert(Ordering.isLT(Ord.compare(DAYS.SUNDAY, DAYS.WEDNESDAY)));
         assert(Ordering.isGT(Ord.compare(DAYS.FRIDAY, DAYS.SUNDAY)));
     });
 
     it('should create a sum type that implements Enum', function() {
+        assert(Enum.member(DAYS.SUNDAY));
+
         assert(_arrEqual(Enum.enumTo(DAYS.TUESDAY, DAYS.FRIDAY), [DAYS.TUESDAY, DAYS.WEDNESDAY, DAYS.THURSDAY, DAYS.FRIDAY]));
         assert(_arrEqual(Enum.enumUntil(DAYS.THURSDAY, DAYS.MONDAY), [DAYS.THURSDAY, DAYS.WEDNESDAY, DAYS.TUESDAY]));
     });
