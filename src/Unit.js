b001e828c7fa2b1989d10d65945c31de7d18f0aa
mwardle <mwardle@robinsage.com>
2016-01-12 23:29:16 -0700

    Added functionality (Monoid, Functor) to Unit

--- src/Unit.js
+++ src/Unit.js
@@ -1,16 +1,17 @@
 var _curry = require('./internal/_curry');
+var _curryN = require('./internal/_curryN');
+var _always = require('./internal/_always');
 var Ordering = require('./Ordering');
 
 Unit = require('./internal/_primitives').Unit;
 
-Unit.eq = _curry(function(left, right) {
-    // null and undefined are the same as far as I am concerned.
-    return true;
-});
+Unit.eq = _curryN(2, _always(true));
 
-Unit.compare = _curry(function(left, right) {
-    return Ordering.EQ;
-});
+Unit.compare = _curryN(2, _always(Ordering.EQ));
+
+Unit.empty = _always(null);
+Unit.concat = _curryN(2, _always(null));
+Unit.map = _curryN(2, _always(null));
 
 Unit.show = _curry(function(unit){
     return String(unit);
--- test/Functor.mocha.js
+++ test/Functor.mocha.js
@@ -10,32 +10,17 @@ describe('Functor', function(){
             var arr = [1,3,5,7];
             assert(Arr.eq(Functor.map(Int.add(1), arr), [2,4,6,8]));
             assert(Arr.eq(Functor.map(Int.add(1))(arr), [2,4,6,8]));
-        });
 
-        it('should throw an exception if given a null value', function(){
-            try {
-                Functor.map(function(){}, null);
-                assert(false);
-            } catch (e) {
-                assert(e instanceof TypeError);
-            }
-        });
-
-        it('should throw an exception if given undefined', function(){
-            try {
-                Functor.map(function(){}, undefined);
-                assert(false);
-            } catch (e) {
-                assert(e instanceof TypeError);
-            }
+            assert.equal(Functor.map(Int.add(1), null), null);
+            assert.equal(Functor.map(Int.add(1), undefined), null);
         });
     });
 
     describe('#member', function(){
         it('should return true for a value that implements function false otherwise', function(){
             assert(Functor.member([]));
-            assert(!Functor.member(null));
-            assert(!Functor.member(undefined));
+            assert(Functor.member(null));
+            assert(Functor.member(undefined));
             assert(!Functor.member(false));
             assert(!Functor.member(1));
         })
