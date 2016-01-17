2fa84639b840f22e955c60395abf2e3e1a58ab37
mwardle <mwardle@robinsage.com>
2016-01-17 16:56:29 -0700

    Added some functionality to Regex

--- src/Regex.js
+++ src/Regex.js
@@ -1,6 +1,28 @@
+var _curry = require('./internal/_curry');
+
 var Regex = require('./internal/_primitives').Regex;
 
-Regex.toString = function(value) {
+/**
+ * Point-free, curried form of regular expression's test method.
+ *
+ * @sig String -> RegExp -> Boolean
+ * @since 0.6.0
+ */
+Regex.test = _curry(function(str, re){
+    return re.test(str);
+});
+
+/**
+ * Point-free, curried form of a regular expression's exec method.
+ *
+ * @sig String -> RegExp -> null|Array
+ * @since 0.6.0
+ */
+Regex.exec = _curry(function(str, re){
+    return re.exec(str);
+});
+
+Regex.show = function(value) {
     return value.toString();
 };
 
