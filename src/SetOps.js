382d815c05bf9df56e96419412ef93319a7c0d16
mwardle <mwardle@robinsage.com>
2016-01-17 23:18:11 -0700

    Added doc block stubs to SetOps

--- src/SetOps.js
+++ src/SetOps.js
@@ -6,9 +6,36 @@ var _isFunction = require('./internal/_isFunction');
 
 var SetOps = module.exports;
 
+/**
+ * Returns a new set with all the keys from set.
+ *
+ * @sig SetOps s => s -> s -> s
+ * @since 0.6.0
+ */
 SetOps.union = _curryN(2, _useModuleMethod('union'));
+
+/**
+ * Returns a new set with all the keys in the left that are not present in the right.
+ *
+ * @sig SetOps s => s -> s -> s
+ * @since 0.6.0
+ */
 SetOps.difference = _curryN(2, _useModuleMethod('difference'));
+
+/**
+ * Returns a new set with all the keys in both the left and right set.
+ *
+ * @sig SetOps s => s -> s -> s
+ * @since 0.6.0
+ */
 SetOps.intersection = _curryN(2, _useModuleMethod('intersection'));
+
+/**
+ * Returns a new set with all the keys in left or right, but not in both.
+ *
+ * @sig SetOps s => s -> s -> s
+ * @since 0.6.0
+ */
 SetOps.symmetricDifference = _curryN(2, _useModuleMethod('symmetricDifference'));
 
 
@@ -18,5 +45,4 @@ SetOps.member = function(value) {
         && _isFunction(M.difference)
         && _isFunction(M.intersection)
         && _isFunction(M.symmetricDifference);
-
 }
