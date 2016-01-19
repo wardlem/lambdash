2c605dbf3b154bd1bcdf4cb17ca76a986374fd62
mwardle <mwardle@robinsage.com>
2016-01-18 23:03:34 -0700

    Update to regex exec so returns array if no match found

--- src/Regex.js
+++ src/Regex.js
@@ -15,11 +15,14 @@ Regex.test = _curry(function(str, re){
 /**
  * Point-free, curried form of a regular expression's exec method.
  *
- * @sig String -> RegExp -> null|Array
+ * Unlike the built in Regex.prototype.exec function, this function will return
+ * an empty array if no matches are found.
+ *
+ * @sig String -> RegExp -> [String]
  * @since 0.6.0
  */
 Regex.exec = _curry(function(str, re){
-    return re.exec(str);
+    return re.exec(str) || [];
 });
 
 Regex.show = function(value) {
